c95f9ef764b860c60118cba219b202baa6e71ea5
Zxcvbnm123456ZZ <69620099+Zxcvbnm123456ZZ@users.noreply.github.com>
2020-09-21 10:34:53 +0800

    Create Helper.cs

---
+++ multibit-hd/mhbd-core/src/Helper.cs
@@ -0,0 +1,12 @@
+using Neo.Cryptography.ECC;
+using Neo.IO;
+using Neo.IO.Json;
+using Neo.SmartContract;
+using Neo.VM.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Array = Neo.VM.Types.Array;
+using Boolean = Neo.VM.Types.Boolean;
+using Buffer = Neo.VM.Types.Buffer;
