55f31bf8b4fdfe0f09e3b57f68be50f9e349f0d3
qiobusi <69621706+qiobusi@users.noreply.github.com>
2020-09-21 10:05:33 +0800

    generate constructor
    generate constructor

--- scripts/Java/src/main/java/com/zilliqa/staking/EventLogEntry.java
+++ scripts/Java/src/main/java/com/zilliqa/staking/EventLogEntry.java
@@ -5,4 +5,15 @@ public class EventLogEntry {
     public String address;
     public String _eventname;
     public EventParam[] params;
+    
+    public EventLogEntry() {
+        
+    }
+    
+    public EventLogEntry(String address, String _eventname, EventParam[] params) {
+        this.address = address;
+        this._eventname = _eventname;
+        this.params = params;
+    }
+    
 }
