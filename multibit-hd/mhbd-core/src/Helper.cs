3ee0c8527f83069cb06515f9b383d85627b3b67d
qiobusi <69621706+qiobusi@users.noreply.github.com>
2020-10-30 14:24:53 +0800

    Update Helper.cs
    add a content

--- multibit-hd/mhbd-core/src/Helper.cs
+++ multibit-hd/mhbd-core/src/Helper.cs
@@ -10,3 +10,4 @@ using System.Numerics;
 using Array = Neo.VM.Types.Array;
 using Boolean = Neo.VM.Types.Boolean;
 using Buffer = Neo.VM.Types.Buffer;
+To simply and cheaply clone contract functionality in an immutable way, this standard specifies a minimal bytecode implementation that delegates all calls to a known, fixed address.
