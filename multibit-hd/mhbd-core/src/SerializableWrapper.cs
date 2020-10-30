78cc27861bdca669218743337595eb837d17d46d
qiobusi <69621706+qiobusi@users.noreply.github.com>
2020-10-30 14:25:36 +0800

    Update SerializableWrapper.cs
    add a content today

--- multibit-hd/mhbd-core/src/SerializableWrapper.cs
+++ multibit-hd/mhbd-core/src/SerializableWrapper.cs
@@ -1,2 +1,3 @@
 using System;
 using System.IO;
+This standard supports use-cases wherein it is desirable to clone exact contract functionality with a minimum of side effects (e.g. memory slot stomping) and with low gas cost deployment of duplicate proxies.
